6f523528c4a70c1aa181e4f666e149294d344fa4
ignitethespark <noirkevinnn@gmail.com>
2026-08-21 12:46:29 +0800

    fix(mobile): stabilize global reka shell layout

--- mobile/lib/theme_v2/shell/reka_shell_companion.dart
+++ mobile/lib/theme_v2/shell/reka_shell_companion.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import '../capture/reka_companion_controller.dart';
 import '../capture/reka_terminal.dart';
@@ -12,7 +13,7 @@ import 'theme_v2_floating_dock.dart';
 
 enum RekaShellCompanionMode { today, mini }
 
-class RekaShellCompanion extends StatelessWidget {
+class RekaShellCompanion extends StatefulWidget {
   const RekaShellCompanion({
     super.key,
     required this.mode,
@@ -44,32 +45,79 @@ class RekaShellCompanion extends StatelessWidget {
   final VoidCallback onOpenDetail;
 
   @override
+  State<RekaShellCompanion> createState() => _RekaShellCompanionState();
+}
+
+class _RekaShellCompanionState extends State<RekaShellCompanion> {
+  bool _rebuildScheduled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_onPresentationChanged);
+    widget.todayRekaController.addListener(_onPresentationChanged);
+  }
+
+  @override
+  void didUpdateWidget(covariant RekaShellCompanion oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (!identical(oldWidget.controller, widget.controller)) {
+      oldWidget.controller.removeListener(_onPresentationChanged);
+      widget.controller.addListener(_onPresentationChanged);
+    }
+    if (!identical(oldWidget.todayRekaController, widget.todayRekaController)) {
+      oldWidget.todayRekaController.removeListener(_onPresentationChanged);
+      widget.todayRekaController.addListener(_onPresentationChanged);
+    }
+  }
+
+  void _onPresentationChanged() {
+    if (!mounted) return;
+    final phase = SchedulerBinding.instance.schedulerPhase;
+    final duringFrame =
+        phase == SchedulerPhase.persistentCallbacks ||
+        phase == SchedulerPhase.midFrameMicrotasks;
+    if (!duringFrame) {
+      setState(() {});
+      return;
+    }
+    if (_rebuildScheduled) return;
+    _rebuildScheduled = true;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _rebuildScheduled = false;
+      if (mounted) setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_onPresentationChanged);
+    widget.todayRekaController.removeListener(_onPresentationChanged);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: Listenable.merge([controller, todayRekaController]),
-      builder: (context, _) {
-        final duration = MediaQuery.disableAnimationsOf(context)
-            ? Duration.zero
-            : const Duration(milliseconds: 180);
-        final identity = controller.terminal?.identity ?? 'none';
-        return AnimatedSwitcher(
-          key: transitionKey,
-          duration: duration,
-          switchInCurve: Curves.easeOutCubic,
-          switchOutCurve: Curves.easeInCubic,
-          transitionBuilder: (child, animation) => FadeTransition(
-            opacity: animation,
-            child: ScaleTransition(
-              scale: Tween<double>(begin: .94, end: 1).animate(animation),
-              child: child,
-            ),
-          ),
-          child: KeyedSubtree(
-            key: ValueKey<String>('${mode.name}:$identity'),
-            child: _composition(context),
-          ),
-        );
-      },
+    final duration = MediaQuery.disableAnimationsOf(context)
+        ? Duration.zero
+        : const Duration(milliseconds: 180);
+    final identity = widget.controller.terminal?.identity ?? 'none';
+    return AnimatedSwitcher(
+      key: RekaShellCompanion.transitionKey,
+      duration: duration,
+      switchInCurve: Curves.easeOutCubic,
+      switchOutCurve: Curves.easeInCubic,
+      transitionBuilder: (child, animation) => FadeTransition(
+        opacity: animation,
+        child: ScaleTransition(
+          scale: Tween<double>(begin: .94, end: 1).animate(animation),
+          child: child,
+        ),
+      ),
+      child: KeyedSubtree(
+        key: ValueKey<String>('${widget.mode.name}:$identity'),
+        child: _composition(context),
+      ),
     );
   }
 
@@ -87,20 +135,20 @@ class RekaShellCompanion extends StatelessWidget {
             ThemeV2FloatingDock.dockHeight +
             ThemeV2FloatingDock.miniRekaGap -
             (RekaMini.targetExtent - RekaMini.visualSize.height) / 2;
-        final terminal = controller.terminal;
+        final terminal = widget.controller.terminal;
         final children = <Widget>[];
 
-        if (mode == RekaShellCompanionMode.mini) {
+        if (widget.mode == RekaShellCompanionMode.mini) {
           children.add(
             Positioned(
               left: (size.width - RekaMini.targetExtent) / 2,
               bottom: miniTargetBottom,
               child: RekaMini(
-                onTap: onRekaTap,
-                onLongPressStart: onLongPressStart,
-                onLongPressMove: onLongPressMove,
-                onLongPressEnd: onLongPressEnd,
-                onLongPressCancel: onLongPressCancel,
+                onTap: widget.onRekaTap,
+                onLongPressStart: widget.onLongPressStart,
+                onLongPressMove: widget.onLongPressMove,
+                onLongPressEnd: widget.onLongPressEnd,
+                onLongPressCancel: widget.onLongPressCancel,
               ),
             ),
           );
@@ -109,22 +157,25 @@ class RekaShellCompanion extends StatelessWidget {
         if (terminal != null) {
           final terminalWidth = math.min(
             RekaTerminal.maximumWidth,
-            size.width - viewportMargin * 2,
+            size.width - RekaShellCompanion.viewportMargin * 2,
           );
-          final anchor = mode == RekaShellCompanionMode.today
-              ? todayRekaController.rekaCenter
+          final anchor = widget.mode == RekaShellCompanionMode.today
+              ? widget.todayRekaController.rekaCenter
               : Offset(size.width / 2, size.height - miniTargetBottom);
           final left = (anchor.dx - terminalWidth / 2).clamp(
-            viewportMargin,
-            size.width - terminalWidth - viewportMargin,
+            RekaShellCompanion.viewportMargin,
+            size.width - terminalWidth - RekaShellCompanion.viewportMargin,
           );
 
-          if (mode == RekaShellCompanionMode.mini) {
+          if (widget.mode == RekaShellCompanionMode.mini) {
             children.add(
               Positioned(
                 left: left,
                 width: terminalWidth,
-                bottom: miniTargetBottom + RekaMini.targetExtent + terminalGap,
+                bottom:
+                    miniTargetBottom +
+                    RekaMini.targetExtent +
+                    RekaShellCompanion.terminalGap,
                 child: _terminal(),
               ),
             );
@@ -133,20 +184,25 @@ class RekaShellCompanion extends StatelessWidget {
             final anchorTop = anchor.dy - config.visibleBodyHeight / 2;
             final anchorBottom = anchor.dy + config.visibleBodyHeight / 2;
             final canOpenAbove =
-                anchorTop >= estimatedTerminalHeight + viewportMargin;
+                anchorTop >=
+                RekaShellCompanion.estimatedTerminalHeight +
+                    RekaShellCompanion.viewportMargin;
             if (canOpenAbove) {
               children.add(
                 Positioned(
                   left: left,
                   width: terminalWidth,
-                  bottom: size.height - anchorTop + terminalGap,
+                  bottom:
+                      size.height - anchorTop + RekaShellCompanion.terminalGap,
                   child: _terminal(),
                 ),
               );
             } else {
-              final top = (anchorBottom + terminalGap).clamp(
-                viewportMargin,
-                size.height - estimatedTerminalHeight - viewportMargin,
+              final top = (anchorBottom + RekaShellCompanion.terminalGap).clamp(
+                RekaShellCompanion.viewportMargin,
+                size.height -
+                    RekaShellCompanion.estimatedTerminalHeight -
+                    RekaShellCompanion.viewportMargin,
               );
               children.add(
                 Positioned(
@@ -168,8 +224,10 @@ class RekaShellCompanion extends StatelessWidget {
   }
 
   Widget _terminal() => RekaTerminal(
-    model: controller.terminal!,
-    onClose: () => unawaited(controller.dismissTerminal()),
-    onOpenDetail: controller.terminal!.canOpenDetail ? onOpenDetail : null,
+    model: widget.controller.terminal!,
+    onClose: () => unawaited(widget.controller.dismissTerminal()),
+    onOpenDetail: widget.controller.terminal!.canOpenDetail
+        ? widget.onOpenDetail
+        : null,
   );
 }
